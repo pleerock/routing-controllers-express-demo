d421f0b298c6100e8843166454c6e7c9dd04b043
Attila Olah <NoNameProvided@users.noreply.github.com>
2017-05-28 12:03:09 +0200

    refactor(Models): user shortand for prop initialization

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,11 +1,8 @@
 export class Category {
 
-    id: number;
-    name: string;
-
-    constructor(id: number, name: string) {
-        this.id = id;
-        this.name = name;
-    }
+    constructor(
+        public id: number,
+        public name: string
+    ) { }
 
 }
--- src/model/Post.ts
+++ src/model/Post.ts
@@ -2,20 +2,11 @@ import {Category} from "./Category";
 
 export class Post {
 
-    id: number;
-    title: string;
-    text: string;
-    createDate: Date = new Date();
-    categories: Category[];
-
-    constructor(id: number,
-                title: string,
-                text: string,
-                categories: Category[]) {
-        this.id = id;
-        this.title = title;
-        this.text = text;
-        this.categories = categories;
-    }
+    constructor(
+        public id: number,
+        public title: string,
+        public text: string,
+        public categories: Category[]
+    ) { }
 
 }
